2f92ff98dbf5da393e074eaab41a61bf1911c3f4
schvabodka-man <scvhapps@gmail.com>
2017-11-14 10:29:26 +0200

    fixes

--- server/running.sh
+++ server/running.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 emacs --daemon=news -q -l ~/.emacs.d.news/init.el
 calibre-server --port 8089 --daemonize --with-library ~/Data/Share/Books
-aria2c --daemon=true --enable-rpc --rpc-listen-all -j50 -D --save-session-interval=120
+aria2c --daemon=true --dir=~/Data/Downloads --enable-rpc --rpc-listen-all -j50 -D --save-session-interval=120
 # transmission-daemon -T --download-dir ~/Data/Share/Downloads
--- server/setup.sh
+++ server/setup.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-sudo sudo apt-get -y update
-sudo sudo apt-get -y upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-sudo sudo apt-get -y install emacs
-sudo sudo apt-get -y install calibre
-sudo sudo apt-get -y install aria2
-# sudo sudo apt-get -y install transmission-daemon
-sudo sudo apt-get -y install syncthing
-sudo sudo apt-get -y install cron
+sudo apt-get -y install emacs
+sudo apt-get -y install calibre
+sudo apt-get -y install aria2
+#sudo apt-get -y install transmission-daemon
+sudo apt-get -y install syncthing
+sudo apt-get -y install cron
 
 mkdir ~/.emacs.d.news/
+mkdir ~/Data/
 cp ./init.el ~/.emacs.d.news/
 mkdir ~/.config/aria2/
 cp ./aria.conf ~/.config/aria2/
@@ -20,10 +21,10 @@ sudo sed 's/#\?\(PermitRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config > sshd.txt
 sudo mv -f sshd.txt /etc/ssh/sshd_config
 sudo sed 's/#\?\(X11Forwarding\s*\).*$/\1 no/' /etc/ssh/sshd_config > sshd.txt
 sudo mv -f sshd.txt /etc/ssh/sshd_config
-sudo sed 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config > sshd.txt
-sudo mv -f sshd.txt /etc/ssh/sshd_config
-sudo sed 's/#\?\(PubkeyAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config > sshd.txt
-sudo mv -f sshd.txt /etc/ssh/sshd_config
+# sudo sed 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config > sshd.txt
+# sudo mv -f sshd.txt /etc/ssh/sshd_config
+# sudo sed 's/#\?\(PubkeyAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config > sshd.txt
+# sudo mv -f sshd.txt /etc/ssh/sshd_config
 sudo sed 's/#\?\(PermitEmptyPasswords \s*\).*$/\1 no/' /etc/ssh/sshd_config > sshd.txt
 sudo mv -f sshd.txt /etc/ssh/sshd_config
 sudo sed 's/#\?\(MaxAuthTries \s*\).*$/\1 3/' /etc/ssh/sshd_config > sshd.txt
