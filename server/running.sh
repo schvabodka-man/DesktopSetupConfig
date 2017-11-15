206acfe7490c45ab6f4cde7de5e401b314f46d91
schvabodka-man <scvhapps@gmail.com>
2017-11-15 09:17:59 +0200

    gitea

--- server/running.sh
+++ server/running.sh
@@ -2,4 +2,4 @@
 emacs --daemon
 calibre-server --port 8089 --daemonize --with-library ~/Data/Share/Books
 aria2c --daemon --enable-rpc --rpc-listen-all --continue=true --save-session-interval=120 --dir=${HOME}/Downloads --no-conf=true --save-session=${HOME}/aria2/session --input-file=${HOME}/aria2/session
-${HOME}/bin/gitea/gitea web
+${HOME}/bin/gitea web
