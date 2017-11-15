395d0eb8644e88fa7f5a1a0a53bb02ae10169e83
schvabodka-man <scvhapps@gmail.com>
2017-11-15 09:04:37 +0200

    aria2

--- server/running.sh
+++ server/running.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-
-currentusername=$(whoami)
-
 emacs --daemon
 calibre-server --port 8089 --daemonize --with-library ~/Data/Share/Books
-aria2c --daemon=true --dir=~/Data/Share/Downloads --enable-rpc --rpc-listen-all -j50 -D --save-session-interval=120 --conf-path=~/.config/aria2/aria2.conf
-cd "/home/$currentusername/bin/gitea/"
-./gitea web
+aria2c --daemon --enable-rpc --rpc-listen-all --continue=true --save-session-interval=120 --dir=${HOME}/Downloads --no-conf=true --save-session=${HOME}/aria2/session --input-file=${HOME}/aria2/session
+${HOME}/bin/gitea/gitea web
--- server/setup.sh
+++ server/setup.sh
@@ -14,6 +14,9 @@ sudo apt-get -y install cron-apt
 
 mkdir ~/.emacs.d/
 mkdir ~/Data/
+mkdir ~/aria2
+touch ~/aria2/session
+
 cp init.el ~/.emacs.d/
 mkdir ~/.config/aria2/
 cp aria2.conf ~/.config/aria2/
@@ -62,5 +65,28 @@ systemctl --user start syncthing.service
 
 sudo sh -c 'echo "/dev/sda1        /home/pi/Data       vfat    uid=1000,gid=1000,umask=0022,sync,auto,nosuid,rw,nouser 0   0" >> /etc/fstab'
 sudo mount -a
+
+#reliable as fuck
+# sudo sh -c 'echo "[Unit]" >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "Description=Gitea (Git with a cup of tea)" >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "After=syslog.target" >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "After=network.target" >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo " " >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "[Service]" >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "LimitMEMLOCK=infinity" >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "LimitNOFILE=65535" >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "Type=simple" >> /etc/systemd/system/gitea.service'
+# sudo sh -c "echo \"User=$currentusername\" >> /etc/systemd/system/gitea.service"
+# sudo sh -c "echo \"Group=git\" >> /etc/systemd/system/gitea.service"
+# sudo sh -c "echo \"WorkingDirectory=/home/$currentusername/bin/gitea\" >> /etc/systemd/system/gitea.service"
+# sudo sh -c "echo \"ExecStart=/home/$currentusername/bin/gitea/gitea web -p 3000\" >> /etc/systemd/system/gitea.service"
+# sudo sh -c "echo \"Restart=always\" >> /etc/systemd/system/gitea.service"
+# sudo sh -c "echo \"Environment=USER=$currentusername HOME=/home/$currentusername\" >> /etc/systemd/system/gitea.service"
+# sudo sh -c 'echo " " >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "[Install]" >> /etc/systemd/system/gitea.service'
+# sudo sh -c 'echo "WantedBy=multi-user.target" >> /etc/systemd/system/gitea.service'
+
+# sudo systemctl enable gitea
+# sudo systemctl start gitea
 
 sudo reboot
